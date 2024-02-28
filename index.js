4a36fc4089d0d906d7350a71b5425978d1bb4419
ariel216 <arielespinoza216@gmail.com>
2024-02-28 00:31:33 -0400

    add: db logic

--- index.js
+++ index.js
@@ -29,16 +29,62 @@ const init = (url)=>{
     )
 }
 
-
 const listarData = async ()=>{
     let consulta = `select * from public.ubicaciones 
-                    where referencia  <> '' or referencia <> null order by id asc limit 5`;    
+                    where referencia  <> '' or referencia <> null
+                    and link  <> '' or link <> null
+                    order by id asc`;    
     let resultados = await conexionPG.query(consulta);
     for (let i = 0; i < resultados.rowCount; i++) {
-        const element = resultados.rows[i];
-        console.log(element.id);   
-        console.log(init(element.referencia));
+      const element = resultados.rows[i];
+      console.log(element.id);   
+      //console.log(getUrl(element.referencia));
+
+      request(
+        {
+          uri: element.referencia,
+          followRedirect: false,
+        },
+        async function (err, httpResponse) {
+          if (err) {
+            return console.error(err)
+          }
+          const unshorted = await httpResponse.headers.location || url; 
+          console.log(unshorted);      
+          consultaInserta = `update public.ubicaciones set link = $2 where id = $1`;
+          await conexionPG.query(consultaInserta, [element.id, unshorted]);
+        },
+      )
     }
 }
 
-listarData();
+const listarLink = async ()=>{
+  let consulta = `select * from public.ubicaciones 
+                  where (referencia  <> '' or referencia <> null)
+                  and (link  <> '' or link <> null) and id <> 860
+                  --order by id asc`;    
+  let resultados = await conexionPG.query(consulta);
+  for (let i = 0; i < resultados.rowCount; i++) {
+    const element = resultados.rows[i];
+    console.log(element.id);   
+    //console.log(getUrl(element.referencia));
+    var splitUrl = element.link.split('!3d');
+    var latLong = splitUrl[splitUrl.length-1].split('!4d');
+    var longitude;
+
+    if (latLong.indexOf('?') !== -1) {
+        longitude = latLong[1].split('?')[0];
+        console.log('split');
+    } else {
+        //longitude = latLong[1];
+        longitude = latLong[1].split('?')[0];
+    }
+    var latitude = latLong[0];
+
+    consultaInserta = `update public.ubicaciones set lat = $2, lon = $3 where id = $1`;
+    await conexionPG.query(consultaInserta, [element.id, latitude, longitude]);
+
+  }
+}
+
+listarLink();
